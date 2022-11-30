b372cc289c05c36b87570f8537fbf266b2b24c0f
Fede-Arevalo <arevalo.feder@gmail.com>
2022-11-30 16:27:55 +0100

    se agregaron todos los componentes básicos para dejar las rutas en App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Register from "./components/Register/Register";
+import Login from "./components/Login/Login";
+import Profile from "./components/Profile/Profile";
 
 function App() {
   return (
