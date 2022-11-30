5e219f0d5f41842ebf17f256cd05ddb946669ebd
Fede-Arevalo <arevalo.feder@gmail.com>
2022-11-30 13:18:18 +0100

    Rutas en App.js, componente Cart, Footer, Product y Product Context básico

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <div className="App">
-    
-  </div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
