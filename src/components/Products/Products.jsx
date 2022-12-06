42c0b0dfec3f927f4279a4912cd5196429a2ad2c
Fede-Arevalo <arevalo.feder@gmail.com>
2022-12-06 19:09:08 +0100

    corrección css de vista products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,5 +1,9 @@
 import React, { useContext, useEffect } from "react";
 import { ProductsContext } from "../../context/ProductsContext/ProductsState";
+import { Button, Card } from "antd";
+import "./Products.scss";
+
+const { Meta } = Card;
 
 const Products = () => {
   const { getProducts, products, addCart, cart } = useContext(ProductsContext);
@@ -15,18 +19,43 @@ const Products = () => {
 
   const product = products.map((product) => {
     return (
-      <div key={product._id}>
-        <h1>{product.name}</h1>
-        <p>{product.Category.name}</p>
-        <p>{product.description}</p>
-        <img src={"http://localhost:3001/" + product.img_product} alt={product.name} />
-        <p>{product.price.toFixed(2)}</p>
-        <button onClick={() => addCart(product)}>Add Cart</button>
+      <div className="card" key={product.id}>
+        <Card
+          hoverable
+          style={{
+            width: 350,
+            height: 600,
+            
+          }}
+          cover={
+            <div className="img-product">
+              <img
+                alt={product.name}
+                src={"http://localhost:3001/" + product.img_product}
+              />
+            </div>
+          }
+        >
+        <div className="text-container">
+          <Meta title={product.name} description={product.description} />
+          </div>
+          <p className="price">{product.price.toFixed(2)} €</p>
+          <Button type="primary" block onClick={() => addCart(product)}>
+            Add Cart
+          </Button>
+        </Card>
       </div>
     );
   });
 
-  return <div>{product}</div>;
+  return (
+    <>
+      <h1>Products</h1>
+      <div className="products">
+        <div className="product-container">{product}</div>
+      </div>
+    </>
+  );
 };
 
 export default Products;
